f9e2424356ebf231f8ff547ec4ba5f07ee6c6723
Johan Blomberg <jccblomberg@gmail.com>
2016-03-11 17:26:45 +0100

    CustomerSort now uses IDSort if the two names are equal

--- src/CustomerSort.java
+++ src/CustomerSort.java
@@ -4,6 +4,13 @@ public class CustomerSort implements Comparator<Order> {
 
 	@Override
 	public int compare(Order o1, Order o2) {
-		return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+		int n = o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+		//If the names are equal, compare IDs instead
+		if(n == 0) {
+			return (new IDSort()).compare(o1, o2);
+		}
+		else {
+			return n;
+		}
 	}
 }
